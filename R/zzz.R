3670695e048d7841f787244340b4b5521efb702c
KasperSkytte <knaldhat@gmail.com>
2018-02-26 17:38:49 +0100

    Added github release check on attach

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,24 @@
 .onAttach <- function(lib, pkg)  {
-  local_version <- utils::packageVersion("mmgenome2")
-  packageStartupMessage("This is ", pkg, " version ", local_version, ". Great documentation is available at the mmgenome2 website: https://madsalbertsen.github.io/mmgenome2/\n", appendLF = TRUE)
-  options(scipen = 6)
+  #Check for new github release version. (Not master branch version, release version!)
+  if (!interactive()) {
+    return()
+  } else {
+    local_version <- utils::packageVersion("mmgenome2")
+    packageStartupMessage("This is ", pkg, " version ", local_version, ". Great documentation is available at the mmgenome2 website: https://madsalbertsen.github.io/mmgenome2/\n", appendLF = TRUE)
+    options(scipen = 6)
+    if(requireNamespace("remotes", quietly = TRUE)) {
+      tryCatch({
+        github_ref <- remotes:::github_resolve_ref(
+          remotes::github_release(), 
+          remotes:::parse_git_repo("madsalbertsen/mmgenome2@*release"))$ref
+        github_version <- package_version(gsub("v", "", github_ref))
+        if(local_version < github_version) {
+          packageStartupMessage(
+            "New release of ", pkg, " (", github_version, ") is available! Install the latest release with: \nremotes::install_github(\"madsalbertsen/mmgenome2@*release\")\n\nRead the release notes at: https://github.com/MadsAlbertsen/mmgenome2/releases/tag/", github_version)
+        }
+      }, error=function(e) {
+        packageStartupMessage("Can't reach GitHub to check for new releases just now. Trying again next time. \n")
+      })
+    }
+  }
 }
