f9bdec16fe5924bacfaec4660a8b84c870cd1ecf
KasperSkytte <knaldhat@gmail.com>
2018-04-16 09:54:15 +0200

    code comments

--- R/mmplot_cov_profiles.R
+++ R/mmplot_cov_profiles.R
@@ -1,15 +1,15 @@
-#' @title Plot all coverage profiles 
+#' @title Compare coverage profiles 
 #' 
-#' @description To look for patterns... WIP
+#' @description Plots all coverage profiles in the data for comparison. This is useful for identifying scaffold patterns across multiple coverage profiles and spot potential contaminants.
 #'
-#' @param mm 
-#' @param normalise 
-#' @param alpha 
-#' @param color_by 
-#' @param color_vector 
-#' @param color_scale_log10 
-#' @param y_scale_log10 
-#' @param plot_lines 
+#' @param mm (\emph{required}) A dataframe loaded with \code{\link{mmload}}.
+#' @param normalise (\emph{Logical}) Normalises each coverage column in \code{mm} to its highest value and multiplies by 100. (\emph{Default: } \code{TRUE})
+#' @param alpha The transparancy of the scaffold points and lines, where 0 is invisible and 1 is opaque. (\emph{Default: } \code{0.6})
+#' @param color_by Color the scaffolds by a variable in \code{mm}. (\emph{Default: } \code{NULL})
+#' @param color_vector The colors from which to generate a color gradient when \code{color_by} is set and the variable is continuous. Any number of colors can be used. (\emph{Default: } \code{c("blue", "green", "red")}) 
+#' @param color_scale_log10 (\emph{Logical}) Log10-scale the color gradient when \code{color_by} is set and the variable is continuous. (\emph{Default: } \code{FALSE})
+#' @param y_scale_log10 (\emph{Logical}) Log10-scale the y axis. (\emph{Default: } \code{FALSE})
+#' @param plot_lines (\emph{Logical}) Connect scaffolds with lines. (\emph{Default: } \code{TRUE})
 #'
 #' @return A ggplot2 object.
 #' 
@@ -39,53 +39,62 @@ mmplot_cov_profiles <- function(mm,
                                 color_scale_log10 = FALSE,
                                 y_scale_log10 = FALSE,
                                 plot_lines = TRUE) {
+  #can currently only color by 1 variable
   if(length(color_by) > 1)
     stop("color_by must be of length 1", call. = FALSE)
+  #find which columns are coverage profiles
   cov_variables <- which(substring(tolower(colnames(mm)), 1, 4) == "cov_")
-  if(isTRUE(normalise))
+  #normalise to the highest value in each coverage profile
+  if(isTRUE(normalise)) {
     mm[,cov_variables] %<>% lapply(function(x) {x/max(x)*100})
+  }
   colnames(mm)[1] <- "scaffold"
+  #make a data frame suited for ggplot2 and merge with the color_by variable if supplied
   gg <- data.table::melt(mm,
                          id.vars = 1,
                          measure.vars = cov_variables,
                          value.name = "Coverage") %>%
     dplyr::left_join(mm[,c(1, which(colnames(mm) %in% color_by))], by = "scaffold")
+  #color_by
   if(!is.null(color_by)) {
     p <- ggplot(gg, 
                 aes_string(x = "variable",
                            y = "Coverage",
                            color = color_by))
+    #if numeric set custom colorscale and breaks if colored by GC
     if(is.numeric(mm[[color_by]])) {
-      if(!isTRUE(color_scale_log10)) {
-        p <- p + 
-          scale_color_gradientn(colors = color_vector, breaks = c(20,40,60,80))
-      } else if(isTRUE(color_scale_log10)) {
-        p <- p + 
-          scale_color_gradientn(colors = color_vector, trans = "log10", breaks = c(20,40,60,80))
-      }
+      p <- p + 
+        scale_color_gradientn(colors = color_vector, 
+                              trans = if(isTRUE(color_scale_log10)) "log10" else "identity",
+                              breaks = if(color_by == "gc") c(20,40,60,80) else waiver())
     }
   } else if(is.null(color_by)) {
     p <- ggplot(gg, 
                 aes_string(x = "variable",
                            y = "Coverage"))
   }
+  #log10 scale y axis
   if(isTRUE(y_scale_log10))
     p <- p + scale_y_log10()
+  #plot lines
   if(isTRUE(plot_lines))
     p <- p + geom_line(aes_string(group = "scaffold"), alpha = alpha)
+  #adjust y axis label when normalise = TRUE
   if(isTRUE(normalise))
     p <- p + ylab("Coverage (normalised to 100)")
-  
+  #set point and line transparancy
   p <- p + 
     geom_point(alpha = alpha)
   
   #theme adjustments
   p <- p +
     theme(axis.text.x = element_text(angle = 90),
+          axis.line = element_line(color = "black"),
           panel.background = element_blank(),
           panel.border = element_blank(),
-          panel.grid.minor = element_blank(),
-          panel.grid.major = element_blank(),
+          panel.grid.minor.x = element_blank(),
+          panel.grid.major.x = element_blank(),
+          panel.grid.major.y = element_line(color = "grey95"),
           legend.key = element_blank()
     ) +
     xlab("")
