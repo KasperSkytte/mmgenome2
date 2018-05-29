a6d2eee37ec2f0f93de911be7f4f9f5dce8caed6
KasperSkytte <knaldhat@gmail.com>
2018-05-29 18:28:55 +0200

    mmstats, added comparison of a genome extraction with its original data

--- R/mmstats.R
+++ R/mmstats.R
@@ -3,6 +3,7 @@
 #' @description Calculates some basic statistics like the N50 length, minimum-, mean-, and maximum scaffold lengths, mean GC content, information about essential genes, and more...
 #'
 #' @param mm (\emph{required}) A dataframe loaded with \code{\link{mmload}}.
+#' @param original_data If \code{mm} is a subset/extraction of another dataframe loaded with \code{\link{mmload}}, then provide here the original dataframe from which the extraction originates to compare the extraction to the original data, see examples. (\emph{Default: } \code{NULL})
 #' @export
 #'
 #' @details 
@@ -24,18 +25,26 @@
 #'
 #' @importFrom dplyr select starts_with
 #' @importFrom tidyr separate_rows
-#' @importFrom magrittr %<>% %>%
+#' @importFrom magrittr %<>% %>% 
 #' 
 #' @examples 
 #' library(mmgenome2)
 #' data(mmgenome2)
-#' mmgenome2
 #' mmstats(mmgenome2)
+#' 
+#' #Compare an extraction with the original data from which the extraction originates:
+#' selection <- data.frame(cov_C13.11.25 = c(7.2, 16.2, 25.2, 23.3, 10.1),
+#'                         cov_C14.01.09 = c(47, 77, 52.8, 29.5, 22.1))
+#' mmgenome2_extraction <- mmextract(mmgenome2, 
+#'                                   selection = selection)
+#'                                   
+#' mmstats(mmgenome2_extraction, original_data = mmgenome2)
 #' 
 #' @author Kasper Skytte Andersen \email{ksa@@bio.aau.dk}
 #' @author Soren M. Karst \email{smk@@bio.aau.dk}
 #' @author Mads Albertsen \email{MadsAlbertsen85@@gmail.com}
-mmstats <- function(mm) {
+mmstats <- function(mm, 
+                    original_data = NULL) {
   mm$length <- as.numeric(mm$length)
   lengthTotal <- sum(mm$length)
   
@@ -49,6 +58,17 @@ mmstats <- function(mm) {
   } else
     ess.genes <- NULL
   
+  #calculate weighted coverage
+  calcCov <- function(x) {
+    unlist(lapply(dplyr::select(x, dplyr::starts_with("cov_")), 
+                  function(cov) {
+                    nonEmptyIDs <- which(complete.cases(cov))
+                    nonEmpty_mm <- x[nonEmptyIDs,]
+                    round(sum(cov[nonEmptyIDs]*nonEmpty_mm$length)/sum(nonEmpty_mm$length), 2)
+                  }))
+  }
+  
+  #make a named vector with the stats
   stats <- c(Scaffolds = length(mm$scaffold),
              N50 = N50,
              Length.total = lengthTotal,
@@ -56,28 +76,40 @@ mmstats <- function(mm) {
              Length.mean = round(mean(mm$length) ,2),
              Length.min = min(mm$length),
              weighted_GC_mean = round(sum(mm$gc*mm$length)/lengthTotal, 2),
-             unlist(lapply(dplyr::select(mm, dplyr::starts_with("cov_")), 
-                           function(x) {
-                             nonEmptyIDs <- which(complete.cases(x))
-                             nonEmpty_mm <- mm[nonEmptyIDs,]
-                             round(sum(x[nonEmptyIDs]*nonEmpty_mm$length)/sum(nonEmpty_mm$length), 2)
-                             })),
+             if(is.null(original_data)) {
+               calcCov(mm)
+             } else if(!is.null(original_data)) {
+               #If original_data is supplied calculate the coverage profiles
+               #as a fraction of the original data coverage profiles
+               round(lengthTotal/sum(original_data$length)*(calcCov(mm)/calcCov(original_data))*100, 2) %>% 
+                 `names<-`(paste0(names(.), " (% of original)"))
+             },
              Ess.genes.total = length(ess.genes),
              Ess.genes.unique = length(unique(ess.genes))
   )
-  df <- data.frame(stats)
-  colnames(df)[1] <- "General stats"
-  df[,1] %<>% 
+  #save the stats in a data frame for export
+  stats %<>% 
+    as.data.frame() %>%
+    `colnames<-`("General stats")
+  
+  #prettyprint the stats data frame
+  stats[,1] %>% 
     as.character() %>%
     prettyNum(big.mark = " ", 
               big.interval = 3, 
               drop0trailing = T,
               nsmall = 2) %>%
     trimws() %>%
-    StrAlign(sep = ".")
-  print.data.frame(df, 
-                   quote = FALSE,
-                   right = TRUE,
-                   row.names = TRUE,
-                   max = nrow(df))
+    StrAlign(sep = ".") %>%
+    as.data.frame() %>%
+    `colnames<-`("General stats") %>%
+    `rownames<-`(rownames(stats)) %>%
+    print.data.frame(
+      quote = FALSE,
+      right = TRUE,
+      row.names = TRUE,
+      max = nrow(.))
+  
+  #return the stats data frame invisibly
+  invisible(stats)
 }
