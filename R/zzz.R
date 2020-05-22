1c4663f609a167b7127e9dee666119bbfca4cb8e
Kapper <knaldhat@gmail.com>
2020-05-22 14:31:17 +0200

    R > 3.5.0+Rtsne from fork (stable)+fix on pkg load version check

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,13 @@
+#' .onAttach
+#'
+#' @param lib lib
+#' @param pkg pkg
+#'
+#' @importFrom utils packageVersion
 .onAttach <- function(lib, pkg) {
   options(scipen = 6)
-  # Check for new github version (master branch).
   if (!interactive()) {
-    return()
+    invisible()
   } else {
     installed_version <- as.character(utils::packageVersion(pkg))
     packageStartupMessage(
@@ -10,11 +15,11 @@
         "This is ",
         pkg,
         " version ",
-        installed_version,
-        ". Great documentation is available at the mmgenome2 website: https://kasperskytte.github.io/mmgenome2/"),
-      appendLF = TRUE)
+        installed_version
+      ),
+      appendLF = FALSE
+    )
     gitHubUser <- "kasperskytte"
-    errMsg <- "Couldn't reach GitHub to check for new version just now."
     tryCatch(
       {
         DESCRIPTION <- readLines(
@@ -29,22 +34,21 @@
         remote_version <- gsub("Version:\\s*", "", DESCRIPTION[grep("Version:", DESCRIPTION)])
         if (installed_version < remote_version) {
           packageStartupMessage(
-            paste0(
-              "\nNewer version of ", 
-              pkg, 
-              " (", 
-              remote_version, 
-              ") is available! Install the latest version with the following command (copy/paste): \nremotes::install_github(\"kasperskytte/mmgenome2\")"),
-            appendLF = TRUE
+            paste0(" (newer version ", remote_version, " available)"),
+            appendLF = FALSE
           )
+        } else if (installed_version >= remote_version) {
+          packageStartupMessage(" (up to date)", appendLF = FALSE)
         }
       },
       error = function(e) {
-        packageStartupMessage(errMsg, appendLF = TRUE)
+        warning("Can't reach GitHub to check for new version just now. Trying again next time.", call. = FALSE)
       },
       warning = function(e) {
-        packageStartupMessage(errMsg, appendLF = TRUE)
+        warning("Can't reach GitHub to check for new version just now. Trying again next time.", call. = FALSE)
       }
     )
+    packageStartupMessage(". Great documentation is available at the mmgenome2 website: https://kasperskytte.github.io/mmgenome2/", appendLF = TRUE)
   }
+  invisible()
 }
